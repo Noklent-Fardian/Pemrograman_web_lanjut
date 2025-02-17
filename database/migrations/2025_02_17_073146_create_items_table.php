fc1181bdd671e0b1683903e029c4c11c1f7c6acf
Noklent <noklen350@gmail.com>
2025-02-17 14:59:30 +0700

    add:item migrations

---
+++ database/migrations/2025_02_17_073146_create_items_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) { // membuat tables dengan nama items
+            $table->id(); // membuat kolom id
+            $table->string('name'); // membuat kolom name
+            $table->text('description'); // membuat kolom description
+            $table->timestamps(); // membuat kolom timestamp
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
