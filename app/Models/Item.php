2f7faa008d074e51b7ff2fe915a943d852ed3a1b
Noklent <noklen350@gmail.com>
2025-02-17 14:59:15 +0700

    chore: add commnet item model

---
+++ app/Models/Item.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    use HasFactory; // 
+    protected $fillable = [ 
+        'name', 
+        'description',
+    ]; // membuat mass assignment atau mengizinkan kolom name dan description untuk diisi pada tabel items
+}
