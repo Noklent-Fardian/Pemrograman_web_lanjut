10c53dbd1efda2799384d207569287ae25796bff
Noklent <noklen350@gmail.com>
2025-02-17 14:59:46 +0700

    chore: routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ItemController; // import controller ItemController
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('items', ItemController::class); // resource controller dengan nama items
