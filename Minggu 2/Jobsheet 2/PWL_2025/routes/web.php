db00414e019ff55331c2a1ea8dee8c9df3baa6f6
Noklent <noklen350@gmail.com>
2025-02-23 23:19:45 +0700

    JS2-Basic Routing

--- Minggu 2/Jobsheet 2/PWL_2025/routes/web.php
+++ Minggu 2/Jobsheet 2/PWL_2025/routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/welcome', function () {
+    return 'Selamat Datang Noklent';
+});
+Route::get('/about', function () {
+    return 'NIM : 2341720082 , Nama : Noklent Fardian';
+});
