598deb5ebd556ba6774a829efb27d0b8e2eafc62
Noklent <noklen350@gmail.com>
2025-02-24 12:57:42 +0700

    POS-Penjualan Section

---
+++ Minggu 2/POS-Noklent-Fardian/resources/views/sales/index.blade.php
@@ -0,0 +1,11 @@
+<!-- resources/views/sales.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Transaksi POS</title>
+</head>
+<body>
+    <h1>Halaman Transaksi POS</h1>
+    <p>Ini adalah halaman untuk melakukan transaksi penjualan.</p>
+</body>
+</html>
