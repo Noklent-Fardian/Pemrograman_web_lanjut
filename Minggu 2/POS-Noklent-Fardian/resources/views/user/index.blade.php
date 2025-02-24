5a95d7a139f9c4c0e9003dbee7dae0163c6a3df9
Noklent <noklen350@gmail.com>
2025-02-24 12:56:27 +0700

    POS-User Section

---
+++ Minggu 2/POS-Noklent-Fardian/resources/views/user/index.blade.php
@@ -0,0 +1,12 @@
+<!-- resources/views/user.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Profil Pengguna</title>
+</head>
+<body>
+    <h1>Profil Pengguna</h1>
+    <p>ID: {{ $id }}</p>
+    <p>Nama: {{ $name }}</p>
+</body>
+</html>
