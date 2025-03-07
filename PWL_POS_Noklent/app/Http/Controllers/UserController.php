40608480d1ba7bfe9d7db1d4db3c1d611d52a125
Noklent <noklen350@gmail.com>
2025-03-07 10:11:15 +0700

    add: JS4-Praktikum 1

---
+++ PWL_POS_Noklent/app/Http/Controllers/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+
+    /*========================= JS 3 ===============================*/
+    // $data = [
+    //     'username' =>'Pelanggan 1'
+    // ];
+    // UserModel::where('username', 'customer-1')->update($data);
+    // $data = UserModel::all(); //mengambil semua data dari tabel m_user
+    // return view('user', ['data' => $data]); 
+
+     /*========================= JS 4 ===============================*/
+    public function index()
+    {
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
+        ];
+        UserModel::create($data);
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }
+}
