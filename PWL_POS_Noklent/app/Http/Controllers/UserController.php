17ef529e76433691566b6d3f3ce50ad12f2f7d89
Noklent <noklen350@gmail.com>
2025-03-07 13:53:21 +0700

    add: JS4-Praktikum 2.1

--- PWL_POS_Noklent/app/Http/Controllers/UserController.php
+++ PWL_POS_Noklent/app/Http/Controllers/UserController.php
@@ -18,17 +18,25 @@ class UserController extends Controller
     // $data = UserModel::all(); //mengambil semua data dari tabel m_user
     // return view('user', ['data' => $data]); 
 
-     /*========================= JS 4 ===============================*/
+    /*========================= JS 4 ===============================*/
     public function index()
     {
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
-        $user = UserModel::all();
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::find(1);
+        // $user = UserModel::Where('level_id', 1)->first();
+        // $user = UserModel::firstWhere('level_id', 1);
+        $user = UserModel::findor(21, ['username', 'nama'], function () {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
