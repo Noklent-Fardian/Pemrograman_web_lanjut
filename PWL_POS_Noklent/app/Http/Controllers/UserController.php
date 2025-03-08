6890ce24850ece04d02281913b732c35e9d1d509
Noklent <noklen350@gmail.com>
2025-03-08 07:00:55 +0700

    Add: JS4-Praktikum 2.3

--- PWL_POS_Noklent/app/Http/Controllers/UserController.php
+++ PWL_POS_Noklent/app/Http/Controllers/UserController.php
@@ -36,9 +36,19 @@ class UserController extends Controller
         // $user = UserModel::firstWhere('level_id', 1);
         // $user = UserModel::findor(21, ['username', 'nama'], function () {
         //     abort(404);
-        // // });
         // $user = UserModel::findOrFail(1);
-        $user = UserModel::where ('username', 'manager9') ->firstOrFail();
-        return view('user', ['data' => $user]);
+
+        try {
+            $users = UserModel::Where('level_id', 2)->get();
+            $userCount = UserModel::where('level_id', 2)->count();
+
+            if ($userCount === 0) {
+                return response()->json(['message' => 'No users found'], 404);
+            }
+
+            return view('user', ['userCount' => $userCount, 'data' => $users]);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException) {
+            return response()->json(['message' => 'No users found'], 404);
+        }
     }
 }
--- PWL_POS_Noklent/resources/views/user.blade.php
+++ PWL_POS_Noklent/resources/views/user.blade.php
@@ -17,14 +17,18 @@
             <th>Username</th>
             <th>Nama</th>
             <th>ID Level Pengguna</th>
+            <th>Jumlah Pengguna</th>
         </tr>
-        {{-- @foreach ($data as $d) --}}
+        @foreach ($data as $d)
             <tr>
-                <td>{{ $data->user_id }}</td>
-                <td>{{ $data->username }}</td>
-                <td>{{ $data->nama }}</td>
-                <td>{{ $data->level_id }}</td>
-            </tr>
-        {{-- @endforeach --}}
+
+                <td>{{ $d->user_id }}</td>
+                <td>{{ $d->username }}</td>
+                <td>{{ $d->nama }}</td>
+                <td>{{ $d->level_id }}</td>
+        @endforeach
+        <td> {{ $userCount }}</td>
+        </tr>
+
     </table>
 </body>
