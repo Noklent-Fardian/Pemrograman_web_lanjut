8c5e6e3596329ba98480df55ee890f2f86dbb4fe
Noklent <noklen350@gmail.com>
2025-04-13 11:23:53 +0700

    chore:change preloader

--- PWL_POS_Noklent_World_616/resources/views/layouts/template.blade.php
+++ PWL_POS_Noklent_World_616/resources/views/layouts/template.blade.php
@@ -33,8 +33,10 @@
 <body class="hold-transition sidebar-mini">
     <!-- Preloader -->
     <div class="preloader flex-column justify-content-center align-items-center">
-        <img class="animation__shake" src="{{ asset('adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
-            height="60" width="60">
+        <div class="preloader-container">
+            <img class="animation__shake" src="{{ asset('img/logo_clean.png') }}" alt="Logo"
+                height="240" width="240">
+        </div>
     </div>
 
     <!-- Site wrapper -->
